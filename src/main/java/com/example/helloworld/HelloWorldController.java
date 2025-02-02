ffe8334c66908a7dab1a553289cf9d907873220e
chenlunwuhui <101497303+chenlunwuhui@users.noreply.github.com>
2025-02-02 19:11:00 +0800

    Update HelloWorldController.java

--- src/main/java/com/example/helloworld/HelloWorldController.java
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -10,7 +10,7 @@ public class HelloWorldController {
 
 	@RequestMapping(value = { "/hello" }, method = RequestMethod.GET)
 	public String sayHelloWorld() {
-		String responseMsg = new StringBuilder("<h1>").append("Hey...Hello World !!").append("</h1>").toString();
+		String responseMsg = new StringBuilder("<h1>").append("Hey...He11111111111orld !!").append("</h1>").toString();
 		return responseMsg;
 	}
 
